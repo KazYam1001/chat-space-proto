f23706ceb7c9ad10375d7be184ed7e30ec7f7acb
Kazushi Yamanishi <keironia187149@gmail.com>
2019-01-08 12:38:17 +0900

    add groups#new groups#create logic

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,14 +1,20 @@
 class GroupsController < ApplicationController
   def index
-
+    @groups = current_user.groups
   end
 
   def new
-
+    @group = Group.new
+    @group.users << current_user
   end
 
   def create
-
+    @group = Group.new(group_params)
+    if @group.save
+      redirect_to root_path, notice: "グループを作成しました"
+    else
+      render :new
+    end
   end
 
   def edit
@@ -22,6 +28,6 @@ class GroupsController < ApplicationController
   private
 
   def group_params
-
+    params[:group].permit(:name, user_ids: [])
   end
 end
