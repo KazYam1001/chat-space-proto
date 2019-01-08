a21fc5e8e0379c822ee4f820ed340773956bb61b
Kazushi Yamanishi <keironia187149@gmail.com>
2019-01-08 14:20:26 +0900

    add messages#index, messages#create

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,15 +1,30 @@
 class MessagesController < ApplicationController
-  def index
+  before_action :set_group
 
+  def index
+    @groups = current_user.groups
+    @messages = @group.messages.includes(:user)
+    @message  = Message.new
   end
 
   def create
-
+    @message = @group.messages.new(message_params)
+    if @message.save
+      redirect_to group_messages_path, noice: "メッセージが送信されました"
+    else
+      @messages = @group.messages.includes(:user)
+      flash.now[:alert] = 'メッセージを入力して下さい'
+      render :index
+    end
   end
 
   private
 
   def message_params
+    params[:message].permit(:content, :image).merge(user_id: current_user.id)
+  end
 
+  def set_group
+    @group = Group.find(params[:group_id])
   end
 end
