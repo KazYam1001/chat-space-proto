680fc9e230049041cc7b0ce12c16474a8c58630f
Kazushi Yamanishi <keironia187149@gmail.com>
2019-01-08 15:03:44 +0900

    Add messages#index view 仮置きのビューを実際のメッセージが表示されるように修正

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -2,7 +2,6 @@ class MessagesController < ApplicationController
   before_action :set_group
 
   def index
-    @groups = current_user.groups
     @messages = @group.messages.includes(:user)
     @message  = Message.new
   end
@@ -10,7 +9,7 @@ class MessagesController < ApplicationController
   def create
     @message = @group.messages.new(message_params)
     if @message.save
-      redirect_to group_messages_path, noice: "メッセージが送信されました"
+      redirect_to group_messages_path, notice: "メッセージが送信されました"
     else
       @messages = @group.messages.includes(:user)
       flash.now[:alert] = 'メッセージを入力して下さい'
