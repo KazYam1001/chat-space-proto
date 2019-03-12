163c739b128d3104add7fb126396da66e071ea06
Kazushi Yamanishi <keironia187149@gmail.com>
2019-03-12 21:09:36 +0900

    Revert "C1"
    This reverts commit 59af6c5fcf687e7a0d069704d8f3facde67ff0db.

--- app/assets/javascripts/messages.js
+++ app/assets/javascripts/messages.js
@@ -1,61 +1,56 @@
 $(document).on('turbolinks:load', function() {
-  // function buildHTML(message) {
-  //   const image = message.image ? `<img src="${message.image}">` : "" ;
-  //   const html = `<div class="message" data-message_id="${message.id}">
-  //                   <div class="message__upper-info">
-  //                     <p class="message__upper-info__talker">${message.user_name}</p>
-  //                     <p class="message__upper-info__date">${message.created_at}</p>
-  //                   </div>
-  //                   <p class="message__text">${message.content}</p>
-  //                   ${image}
-  //                 </div>`
-  //   return html;
-  // }
+  function buildHTML(message) {
+    const image = message.image ? `<img src="${message.image}">` : "" ;
+    const html = `<div class="message" data-message_id="${message.id}">
+                    <div class="message__upper-info">
+                      <p class="message__upper-info__talker">${message.user_name}</p>
+                      <p class="message__upper-info__date">${message.created_at}</p>
+                    </div>
+                    <p class="message__text">${message.content}</p>
+                    ${image}
+                  </div>`
+    return html;
+  }
 
-  // let timerId
-  // const path = window.location.pathname;
-  // const groupId  = $('.main-header__left-box__current-group').data('group_id');
+  let timerId
+  const path = window.location.pathname;
+  const groupId  = $('.main-header__left-box__current-group').data('group_id');
 
-  // document.addEventListener("turbolinks:visit", function(){
-  //   clearInterval(timerId);
-  // });
+  document.addEventListener("turbolinks:visit", function(){
+    clearInterval(timerId);
+  });
 
   // メッセージ送信
   $('#new_message').on('submit', function(e) {
     e.preventDefault();
+    const formData = new FormData(this);
 
-    var hoge = $('#message_content').val() + "様";
+    $.ajax({
+      url: path,
+      type: 'POST',
+      data: formData,
+      dataType: 'json',
+      processData: false,
+      contentType: false
+    })
+    .done(function(message) {
+      if (message.content == "" && message.image == null) {
+        alert('メッセージを入力して下さい');
+      } else {
+        $('#new_message')[0].reset();
+        $('.messages').append(buildHTML(message));
+        $('.messages').animate({
+          scrollTop: $('.messages')[0].scrollHeight
+        }, 200);
 
-    $('.messages').append(hoge)
-
-    // const formData = new FormData(this);
-
-    // $.ajax({
-    //   url: path,
-    //   type: 'POST',
-    //   data: formData,
-    //   dataType: 'json',
-    //   processData: false,
-    //   contentType: false
-    // })
-    // .done(function(message) {
-    //   if (message.content == "" && message.image == null) {
-    //     alert('メッセージを入力して下さい');
-    //   } else {
-    //     $('#new_message')[0].reset();
-    //     $('.messages').append(buildHTML(message));
-    //     $('.messages').animate({
-    //       scrollTop: $('.messages')[0].scrollHeight
-    //     }, 200);
-
-    //   }
-    // })
-    // .fail(function() {
-    //   alert("通信に失敗しました");
-    // })
-    // .always(function() {
-    //   $(".submit-btn").prop('disabled', false)
-    // });
+      }
+    })
+    .fail(function() {
+      alert("通信に失敗しました");
+    })
+    .always(function() {
+      $(".submit-btn").prop('disabled', false)
+    });
   });
 
   // 自動更新
